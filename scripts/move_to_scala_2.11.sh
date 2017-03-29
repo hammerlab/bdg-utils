3079eff92085ad8399ef2722c29a5f4ba212099a
Ryan Williams <ryan.blake.williams@gmail.com>
2017-03-29 04:17:54 +0000

    rm all but cli

--- scripts/move_to_scala_2.11.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set +x
-
-find . -name "pom.xml" -exec sed -e "s/2.10.6/2.11.8/g" -e "s/2.10/2.11/g" -i .2.11.bak '{}' \;
-# keep maven-javadoc-plugin at version 2.10.4
-find . -name "pom.xml" -exec sed -e "s/2.11.4/2.10.4/g" -i.2.11.2.bak '{}' \;
-find . -name "*.2.11.*bak" -exec rm {} \;
